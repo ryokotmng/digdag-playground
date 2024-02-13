37799fa480b80b5b20c0b2e175c7d7022373d4bb
RYOKO TOMINAGA <ryoko.tominaga@E728299.local>
2024-02-13 18:06:21 +0900

    add second python task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,12 @@
+import digdag
+
 class MyWorkflow(object):
     def my_task(self, arg: str) -> str:
         print('Running my_task')
         print(arg)
-        # digdag.env.store({'my_task': arg})
+        digdag.env.store({'my_task': arg})
+
+    def my_second_task(self) -> str:
+        print('Running my_second_task')
+        # print(my_task)
+        # digdag.env.params['my_task']
