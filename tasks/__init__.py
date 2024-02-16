2c2c7e7720a3c1445b893b694ecd73223af1b356
RYOKO TOMINAGA <ryoko.tominaga@E728299.local>
2024-02-16 13:12:47 +0900

    new nested bug pattern

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,12 +1,13 @@
 import digdag
 
 class MyWorkflow(object):
-    def my_task(self, arg: str) -> str:
+    def my_task(self, arg: str, nested: str):
         print('Running my_task')
         print(arg)
+        print("nested: "+nested)
         digdag.env.store({'my_task': arg})
 
-    def my_second_task(self, my_task) -> str:
+    def my_second_task(self, my_task):
         print('Running my_second_task')
         print(my_task)
         print(digdag.env.params['my_task'])
