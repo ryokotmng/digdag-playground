ecb8f5263d1ec14023bc972110ad3ee062644f02
RYOKO TOMINAGA <ryoko.tominaga@E728299.local>
2024-02-16 14:16:55 +0900

    unexpandable pattern

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -8,8 +8,9 @@ class MyWorkflow(object):
         digdag.env.store({'my_task': arg})
         digdag.env.store({'arg': arg})
 
-    def my_second_task(self, my_task, arg):
+    def my_second_task(self, my_task, arg, arg2):
         print('Running my_second_task')
         print(my_task)
         print(arg)
+        print(arg2)
         print(digdag.env.params['my_task'])
