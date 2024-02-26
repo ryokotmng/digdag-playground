d9b9edc3dd18e5d27ce96da5d8e2e277c92cd31f
RYOKO TOMINAGA <ryoko.tominaga@E728299.local>
2024-02-26 16:20:19 +0900

    comment out unused tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,17 +1,17 @@
 import digdag
 
 class MyWorkflow(object):
-    def my_task(self, arg: str, nested: str):
+    def my_task(self, arg, nested, who):
         print('Running my_task')
         print(arg)
         print("nested: "+nested)
         digdag.env.store({'my_task': arg})
-        digdag.env.store({'expand': arg})
+        digdag.env.store({'who': who})
         digdag.env.store({'arg': arg})
 
-    def my_second_task(self, my_task, arg, arg2):
+    def my_second_task(self, my_task, arg, who):
         print('Running my_second_task')
         print(my_task)
         print(arg)
-        print(arg2)
+        print(who)
         print(digdag.env.params['my_task'])
